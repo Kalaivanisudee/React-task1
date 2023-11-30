ea3020d5b3847ee423e12a5b7e876f4e080ad2f9
kalaivanisudee <gkalaisakthi22@gmail.com>
2023-11-30 10:50:19 +0530

    Pricecard

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Card1 from './Card1.jsx'
+import Card2 from './Card2.jsx'
+import Card3 from './Card3.jsx'
+
+
+function App() {
+  return (
+    <>
+    <Card1 />
+    <Card2 />
+    <Card3 />
+    </>
+  );
+}
+
+export default App
---
+++ src/Card2.jsx
@@ -0,0 +1,26 @@
+
+
+function Card() {
+    return (
+      <div className="card"> 
+     <ul className="price">
+      <li className="header"> Plus </li>
+      <li className="grey">$9 /months </li>
+      <li>✓ 5 user</li>
+      <li>✓ 50GB storage</li>
+      <li>✓ Unlimited public projects</li>
+      <li>✓ Community Access</li>
+      <li>✓ unlimited private projects</li>
+      <li>✓ Dedicated phone support</li>
+      <li>✓ Free subdomain</li>
+      <li>✗ monthly status Reports</li>
+      <li className="grey"><a href="#" className="Button">Button</a></li>
+  
+  
+     </ul>
+     
+      </div>
+    )
+  }
+  
+  export default Card
